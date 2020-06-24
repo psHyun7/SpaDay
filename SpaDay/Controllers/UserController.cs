5f686ad0ea05da9407d63773b0df86e5a4072d60
Sung Hyun Park <45052946+psHyun7@users.noreply.github.com>
2020-06-24 18:19:47 -0400

    Finished Ch 14 Studio

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add")]
+        public IActionResult Add(User newUser, string confirm)
+        {
+            ViewBag.user = newUser;
+            if (newUser.Password == confirm)
+            {
+                return View("index");
+            }
+            else
+            {
+                ViewBag.error = "The Passwords do not match";
+                return View();
+            }
+            
+        }
+    }
+}
